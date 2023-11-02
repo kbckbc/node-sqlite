e094f41f4c0e39005845499f1bcae1eacc326661
Byeongchan Gwak <kbckbc@gmail.com>
2023-11-02 14:22:01 +0900

    change db.conn() to Promise

--- routes/sqlite.js
+++ routes/sqlite.js
@@ -36,12 +36,6 @@ router.get('/selectfirst', (req, res) => {
   selectFirstRow(res);
 });
 
-router.get('/selecteach', (req, res) => {
-  console.log('/selecteach');
-
-  selectEachRows(res);
-});
-
 router.get('/delete', (req, res) => {
   console.log('/delete');
 
@@ -49,77 +43,95 @@ router.get('/delete', (req, res) => {
 });
 
 function initTable(res) {
-  db.close().then((para) => {
-    console.log('yay:', para);
+  db.close().then((conn) => {
+    console.log('yay:', conn);
     selectAllRows(res); 
-  });  
+  }).catch((e) => {
+    console.error(e.message); // "oh, no!"
+  })
 }
 
 function insertRow(res) {
   const [name, age] = ['sammy', 1];
-  db.conn().run(
-    QUERY.insert,
-    [name, age],
-    (err) => {
-      if (err) {
-        console.error(err.message);
+  db.conn().then((conn) => {
+    conn.run(
+      QUERY.insert,
+      [name, age],
+      (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log(`Inserted a row with the ID: ${this.lastID}`);
+  
+        selectAllRows(res);
       }
-      console.log(`Inserted a row with the ID: ${this.lastID}`);
-
-      selectAllRows(res);
-    }
-  );
+    );    
+  }).catch((e) => {
+    console.error(e.message); // "oh, no!"
+  })
 }
 
 function selectAllRows(res) {
-  db.conn().all(QUERY.select, [], (err, rows) => {
-    if (err) {
-      throw new Error(err.message);
-    }
-    console.log(`rows ${rows}`)
-    if( rows.length == 0 ) {
-      console.log(`empty`);
-    }
-    else {
-      rows.forEach((row) => {
-        console.log(`${row.name}: ${row.age}`);
-      });
-    }
-
-    console.log(`rows [${rows}]`, JSON.stringify(rows));
-
-    res.render('sqlite',{data:rows});
-  });
+  db.conn().then((conn) => {
+    conn.all(QUERY.select, [], (err, rows) => {
+      if (err) {
+        throw new Error(err.message);
+      }
+      console.log(`rows ${rows}`)
+      if( rows.length == 0 ) {
+        console.log(`empty`);
+      }
+      else {
+        rows.forEach((row) => {
+          console.log(`${row.name}: ${row.age}`);
+        });
+      }
+  
+      console.log(`rows [${rows}]`, JSON.stringify(rows));
+  
+      res.render('sqlite',{data:rows});
+    })
+  }).catch((e) => {
+    console.error(e.message); // "oh, no!"
+  })
 }
 
 function selectFirstRow(res) {
   // first row only
-  db.conn().get(QUERY.select, [], (err, row) => {
-    if (err) {
-      throw new Error(err.message);
-    }
-
-    if(row != undefined) {
-      res.render('sqlite',{data:[row]});
-    }
-    else {
-      res.render('sqlite',{data:[]});
-    }
-  });
+  db.conn().then((conn) => {
+    conn.get(QUERY.select, [], (err, row) => {
+      if (err) {
+        throw new Error(err.message);
+      }
+  
+      if(row != undefined) {
+        res.render('sqlite',{data:[row]});
+      }
+      else {
+        res.render('sqlite',{data:[]});
+      }
+    });    
+  }).catch((e) => {
+    console.error(e.message); // "oh, no!"
+  })
 }
 
 function deleteRow(res) {
-  db.conn().run(
-    QUERY.delete,
-    (err) => {
-      if (err) {
-        console.error(err.message);
+  db.conn().then((conn) => {
+    conn.run(
+      QUERY.delete,
+      (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log(`deleted a last row`);
+  
+        selectAllRows(res);
       }
-      console.log(`deleted a last row`);
-
-      selectAllRows(res);
-    }
-  );
+    );    
+  }).catch((e) => {
+    console.error(e.message); // "oh, no!"
+  })
 }
 
 module.exports = router;
